26996afb225c7f863e761f4b732807859b534e4d
Priyadarshan <104220955+prydsn121@users.noreply.github.com>
2025-11-04 23:48:18 +0530

    Comment out user seed script

--- backend/src/seeds/user.seed.js
+++ backend/src/seeds/user.seed.js
@@ -1,4 +1,4 @@
-import { config } from "dotenv";
+/*import { config } from "dotenv";
 import { connectDB } from "../lib/db.js";
 import User from "../models/user.model.js";
 
@@ -112,4 +112,4 @@ const seedDatabase = async () => {
 };
 
 // Call the function
-seedDatabase();
+seedDatabase();*/
